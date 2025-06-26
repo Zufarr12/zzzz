ac0107ec875a6af38706e7bcc3aa7cdad145229e
Zufarr12 <zufar8496@gmail.com>
2025-06-26 09:37:12 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import data from '../public/data'
+import './App.css'
+import Card from './componets/Card'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Users</h1>
+      <div className="ota">
+        {data.map((odam,i)=>{
+          return <Card key={i} ism={odam.name} kasb={odam.position}></Card>
+        })}
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
